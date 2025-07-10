964417d6a0287105e315be4b97a033cf3b615793
Koby Mizrahy <kobymiz@towersemi.com>
2025-07-11 00:40:56 +0300

    fix get-url lambda

--- SERVER/Lambda/src/short-urls/get/index.mjs
+++ SERVER/Lambda/src/short-urls/get/index.mjs
@@ -1,26 +1,45 @@
 // src/get/index.js
-import { docClient, TABLE } from '../shared.js';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import {
+  DynamoDBDocumentClient,
+  GetCommand,
+} from '@aws-sdk/lib-dynamodb';
+import { error } from 'console';
+
+import { HttpStatusCode, getResponseSuccess, getResponseError } from 'request-response-helpers';
+
+// Initialize DynamoDB client and DocumentClient
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+// Define your table and index names
+export const TABLE = process.env.TABLE_NAME;
 
 export const handler = async (event) => {
   const { key } = event.pathParameters;
-  const username = event.requestContext.authorizer.username;
+  console.log('Received key:', key);
 
   try {
-    const { Item } = await docClient.get({
+    const command = new GetCommand({
       TableName: TABLE,
-      Key: { key }
-    }).promise();
-
-    if (!Item) {
-      return { statusCode: 404, body: 'Not found' };
+      Key: { key },
+    });
+    const { Item } = await docClient.send(command);
+    if(!Item) {
+      console.error('Short URL not found for key:', key);
+      var errorResponse = getResponseError(HttpStatusCode.NOT_FOUND, "Short URL not found");
+      errorResponse.headers['Access-Control-Allow-Methods'] = 'GET,OPTIONS';
+      errorResponse.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization';
+      console.log('Response:', errorResponse);
+      return errorResponse;
     }
-    if (Item.username !== username) {
-      return { statusCode: 403, body: 'Forbidden' };
-    }
 
-    return { statusCode: 200, body: JSON.stringify(Item) };
+    const response = getResponseSuccess(JSON.stringify(Item));
+    console.log('Response:', response);
+    return response;
+    
   } catch (err) {
     console.error(err);
-    return { statusCode: 500, body: 'Internal server error' };
+    return getResponseError(HttpStatusCode.INTERNAL_SERVER_ERROR, "Failed to get URL");
   }
 };
