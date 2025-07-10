e503f6d320cb21111a1351ad1eb69347c5b44b02
Koby Mizrahy <kobymiz@towersemi.com>
2025-07-11 00:40:15 +0300

    Delete unused files

--- SERVER/Lambda/src/short-urls/shared.mjs
+++
@@ -1,6 +0,0 @@
-// src/shared.js
-import AWS from 'aws-sdk';
-
-export const docClient = new AWS.DynamoDB.DocumentClient();
-export const TABLE = process.env.TABLE_NAME;
-export const USER_IDX = process.env.USERNAME_INDEX; // only used by list
